3a5ceb6810da396c1b3873ad9b04647b5063bea8
Eduard <edick2081n@yahoo.com>
2023-09-01 17:06:24 +0300

    Реализация административной части

--- selection/admin.py
+++ selection/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from .models import Author, Book
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['name', 'authors_name']
+
+    def authors_name(self, book):
+        authors = []
+        for author in book.book_author.all():
+            authors.append(author.name)
+        return ", ".join(authors)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'book_count']
+
+    def book_count(self, author):
+        books = []
+        for book in author.book_set.all():
+            books.append(book.id)
+        return str(len(books))
+
+
 # Register your models here.
-admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
